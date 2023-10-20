347c98b0a16d45a1fb05a9d2d7c24f60a09908b5
michiko lange taguchi <51973693+michi1008@users.noreply.github.com>
2023-10-19 20:02:49 -0700

    delete API running

--- backend/server.js
+++ backend/server.js
@@ -46,9 +46,6 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   const __dirname = path.resolve();
   app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
 }
 
 app.use(notFound);
